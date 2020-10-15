b45d0b1fc7fdcce7037375a3127e1380a31acb15
whdabai <1733886703@qq.com>
2020-10-15 21:36:27 +0800

    Signed-off-by: whdabai <1733886703@qq.com>

--- vue/components/db-button.vue
+++ vue/components/db-button.vue
@@ -1,10 +1,8 @@
 <template>
     <a :href="href" :class="cls" @click.stop="clickBtn" onclick="return !1">
-        <slot></slot>
-        <span :class="icon" v-if="icon"></span>
+        <slot></slot><span :class="icon" v-if="icon"></span>
     </a>
 </template>
-<!-- slot和span 两个元素要保持在一行，否则会在后面有个空格-->
 
 <script>
     module.exports = {
@@ -296,6 +294,7 @@
     a {
         white-space: nowrap;
         display: inline-block;
+        line-height: normal;
     }
 
     a:hover {
