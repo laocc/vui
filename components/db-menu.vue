aec3ed08414ab967bd33083b9a66f68ad57b746c
ddd <ddd@qqq.com>
2021-02-09 13:50:17 +0800

    Signed-off-by: ddd <ddd@qqq.com>

--- components/db-menu.vue
+++ components/db-menu.vue
@@ -5,23 +5,23 @@
             <li class="dm-head" v-if="!m.item" :class="{active:tabs.index===a+''}">
                 <template v-if="type==='href'">
                     <a :target="m.target" v-if="m.target" :href="m.uri" :style="m.css||''">
-                        <em :class="m.icon"></em>{{m.title}}
+                        <em :class="m.icon"></em>{{ m.title }}
                     </a>
                     <a v-else-if="m.title" @click="clkMenu(a+'',m.uri,m.title,m.sandbox)" :style="m.css||''"
                        onclick="return !1;"
                        :href="m.uri">
-                        <em :class="m.icon"></em>{{m.title}}
+                        <em :class="m.icon"></em>{{ m.title }}
                     </a>
                 </template>
                 <template v-else>
                     <span @click="clkMenu(a+'',m.uri,m.title,m.sandbox)" :style="m.css||''">
-                        <em :class="m.icon"></em>{{m.title}}
+                        <em :class="m.icon"></em>{{ m.title }}
                     </span>
                 </template>
             </li>
 
             <li class="dm-group-title" v-else-if="m.display!==false" @click="reGroup(a)" :style="m.css||''">
-                <em :class="m.icon"></em>{{m.title}}
+                <em :class="m.icon"></em>{{ m.title }}
             </li>
 
             <li class="dm-group-body" v-if="m.item && m.display!==false" :class="{hidden:(group!==a && open>=0)}">
@@ -29,16 +29,16 @@
                     v-show="t.display!==false && t.display!==0 && t.display!=='none'">
                     <dt class="da" v-if="t && type==='href'" :class="{active:tabs.index===a+'_'+b}">
                         <a :target="t.target" v-if="t.target" :href="t.uri" :style="t.css||''">
-                            <em :class="t.icon"></em>{{t.title}}
+                            <em :class="t.icon"></em>{{ t.title }}
                         </a>
                         <a v-else-if="t.title" @click="clkMenu(a+'_'+b,t.uri,t.title,t.sandbox)" :style="t.css||''"
                            onclick="return !1;" :href="t.uri">
-                            <em :class="t.icon"></em>{{t.title}}
+                            <em :class="t.icon"></em>{{ t.title }}
                         </a>
                     </dt>
                     <dt class="db" v-else @click="clkMenu(a+'_'+b,t.uri,t.title,t.sandbox)" :style="t.css||''"
                         :class="{active:tabs.index===a+'_'+b}">
-                        <em :class="t.icon"></em>{{t.title}}
+                        <em :class="t.icon"></em>{{ t.title }}
                     </dt>
                 </dl>
             </li>
@@ -47,104 +47,136 @@
 </template>
 
 <script>
-    module.exports = {
-        props: {
-            width: {
-                type: String,
-                default: '150px',
-                validator(v) {
-                    return /^\d+px$/i.test(v);
-                }
-            },
-            color: {
-                type: Object,
-                validator(v) {
-                    return v.background && v.active;
-                }
-            },
-            background: {
-                type: String,
-                default: '#545c64',
-                validator(v) {
-                    return /^#([a-f0-9]{3}|[a-f0-9]{6})$/i.test(v);
-                }
-            },
+module.exports = {
+    props: {
+        width: {
             type: String,
-            open: {
-                type: Number,
-                default: -1
-            },
-            menu: {
-                type: Array,
-                required: !0
-            },
-            tabs: Object,
+            default: '150px',
+            validator(v) {
+                return /^\d+px$/i.test(v);
+            }
+        },
+        color: {
+            type: Object,
+            validator(v) {
+                return v.background && v.active;
+            }
+        },
+        background: {
+            type: String,
+            default: '#545c64',
+            validator(v) {
+                return /^#([a-f0-9]{3}|[a-f0-9]{6})$/i.test(v);
+            }
+        },
+        type: String,
+        open: {
+            type: Number,
+            default: -1
+        },
+        menu: {
+            type: Array,
+            required: !0
+        },
+        tabs: Object,
 
-        },
-        data() {
-            return {
-                cls: 'active',
-                group: this.open
-            }
-        },
-        created() {
-            // console.log(menu);
-        },
-        computed: {
-            wid: function () {
-                return this.width + 'px';
-            }
-        },
-        watch: {
-            tabs: function (a, b) {
-                console.log(a);
-            }
-        },
-        methods: {
-            newTab(tabs) {
-                this.tabs.keys.length = 0;
-                this.tabs.keys.push('0');
-            },
-            clkMenu(key, uri, title, sandbox) {
-                if (!this.tabs) return !0;
-                if (sandbox === undefined) sandbox = 0;
-                // console.log(key, this.tabs.keys);
-
+    },
+    data() {
+        return {
+            cls: 'active',
+            group: this.open
+        }
+    },
+    created() {
+        // console.log(menu);
+    }, mounted() {
+        this.menu.forEach((m1, a) => {
+            console.log(m1);
+            if (m1.item) {
+                m1.item.forEach((m2, b) => {
+                    if (m2.preload) {
+                        let key = a + '_' + b;
+                        if (this.tabs.keys.indexOf(key) < 0) {
+                            this.tabs.keys.push(key);
+                            this.tabs.items.push({
+                                title: m2.title,
+                                name: key,
+                                content: this.$iframe(m2.uri, !!m2.sandbox)
+                            });
+                        }
+                    }
+                })
+            } else if (m1.preload) {
+                let key = a + '';
+                // a+'',m.uri,m.title,m.sandbox
                 if (this.tabs.keys.indexOf(key) < 0) {
                     this.tabs.keys.push(key);
-                    this.tabs.items.push({title: title, name: key, content: this.$iframe(uri, !!sandbox)});
+                    this.tabs.items.push({
+                        title: m1.title,
+                        name: key,
+                        content: this.$iframe(m1.uri, !!m1.sandbox)
+                    });
                 }
-                this.tabs.index = key;
-                return !1;
-            },
-            requestUrl() {
-                const self = this;
-                self.$request(self.url).then(
-                    function (resp) {
-                        let back = function () {
-                            if (resp.reload || resp.self === 'reload') {
-                                location.reload();
-                            } else {
-                                let u = (resp.jump || resp.href);
-                                if (u) location.href = u;
-                            }
-                        };
-                        if (resp.message) {
-                            self.$alert(resp.message, {callback: back});
-                        } else {
-                            back();
-                        }
-                    },
-                    function (resp) {
-                        self.$alert(resp.message);
-                    }
-                );
-            },
-            reGroup(i) {
-                this.group = i;
             }
+        })
+
+    },
+    computed: {
+        wid: function () {
+            return this.width + 'px';
+        }
+    },
+    watch: {
+        tabs: function (a, b) {
+            console.log(a);
+        }
+    },
+    methods: {
+        newTab(tabs) {
+            this.tabs.keys.length = 0;
+            this.tabs.keys.push('0');
         },
-    }
+        clkMenu(key, uri, title, sandbox) {
+            if (!this.tabs) return !0;
+            if (sandbox === undefined) sandbox = 0;
+            // console.log(key, this.tabs.keys);
+
+            if (this.tabs.keys.indexOf(key) < 0) {
+                this.tabs.keys.push(key);
+                console.log({title: title, name: key, content: this.$iframe(uri, !!sandbox)});
+                this.tabs.items.push({title: title, name: key, content: this.$iframe(uri, !!sandbox)});
+            }
+            this.tabs.index = key;
+            return !1;
+        },
+        requestUrl() {
+            const self = this;
+            self.$request(self.url).then(
+                function (resp) {
+                    let back = function () {
+                        if (resp.reload || resp.self === 'reload') {
+                            location.reload();
+                        } else {
+                            let u = (resp.jump || resp.href);
+                            if (u) location.href = u;
+                        }
+                    };
+                    if (resp.message) {
+                        self.$alert(resp.message, {callback: back});
+                    } else {
+                        back();
+                    }
+                },
+                function (resp) {
+                    self.$alert(resp.message);
+                }
+            );
+        },
+        reGroup(i) {
+            this.group = i;
+        }
+    },
+}
 </script>
 <!--http://debug.vue.com/vue/components-->
 <style scoped>
--- components/db-radio.vue
+++ components/db-radio.vue
@@ -10,12 +10,12 @@
                          :style="key==value? 'color:'+color : 'color:#333333' "
                     ></div>
                     <div :style="key==value?'color:'+color : 'color:#333333' ">
-                        {{lab}}
+                        {{ lab }}
                     </div>
                 </template>
 
                 <template v-else>
-                    <div :style="key==value?'color:#ffffff;background:'+color:'color:'+color">{{lab}}</div>
+                    <div :style="key==value?'color:#ffffff;background:'+color:'color:'+color">{{ lab }}</div>
                 </template>
             </li>
         </ul>
@@ -24,150 +24,155 @@
 </template>
 
 <style scoped>
-    div.body {
-        flex: 1;
-        display: flex;
-        margin-left: 1px;
-    }
+div.body {
+    flex: 1;
+    display: flex;
+    margin-left: 1px;
+}
 
-    ul {
-        display: flex;
-        flex: 1;
-        flex-wrap: wrap;
-    }
+ul {
+    display: flex;
+    flex: 1;
+    flex-wrap: wrap;
+}
 
-    ul li {
-        cursor: default;
-        vertical-align: middle;
-        min-width: 60px;
-        align-items: stretch;
-        display: flex;
-        justify-content: flex-start;
-        border-radius: 1px;
-        /*background: #ffffff;*/
-        min-height: 32px;
-        font-size: 14px;
-    }
+ul li {
+    cursor: default;
+    vertical-align: middle;
+    min-width: 60px;
+    align-items: stretch;
+    display: flex;
+    justify-content: flex-start;
+    border-radius: 1px;
+    /*background: #ffffff;*/
+    min-height: 32px;
+    font-size: 14px;
+}
 
-    ul.button li div {
-        flex: 1;
-        display: flex;
-        justify-content: center;
-        padding: 0 9px;
-        height: auto;
-        align-items: center;
-    }
+ul.button li div {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    padding: 0 9px;
+    height: auto;
+    align-items: center;
+}
 
-    ul.button li:last-child {
-        border-right: 1px solid #ddd;
-    }
+ul.button li:last-child {
+    border-right: 1px solid #ddd;
+}
 
-    ul.button li:hover {
-        background: #eee9c6;
-    }
+ul.button li:hover {
+    background: #eee9c6;
+}
 
-    ul.radio li:hover {
-        background: #eee9c6;
-    }
+ul.radio li:hover {
+    background: #eee9c6;
+}
 
-    ul.radio li .fc {
-        font-size: 22px;
-    }
+ul.radio li .fc {
+    font-size: 22px;
+}
 
-    ul.radio li .f3a6 {
-        transition: 1s;
-    }
+ul.radio li .f3a6 {
+    transition: 1s;
+}
 
-    ul.radio li div {
-        align-items: center;
-        height: auto;
-        display: flex;
-        padding: 0 3px;
-    }
+ul.radio li div {
+    align-items: center;
+    height: auto;
+    display: flex;
+    padding: 0 3px;
+}
 
-    ul li.active {
+ul li.active {
 
-    }
+}
 </style>
 
 <script>
-    module.exports = {
-        props: {
-            value: {
-                type: [String, Number],
-                default: ''
-            },
-            color: {
-                type: String,
-                default: '#00a65a'
-            },
-            type: {
-                type: String,
-                default: 'radio',
-                validator: function (value) {
-                    return ['radio', 'button'].indexOf(value) !== -1
-                }
-            },
-            field: {
-                type: String,
-                default: ''
-            },
-            label: {
-                type: String,
-                default: ''
-            },
-            name: {
-                type: String,
-                default: ''
-            },
-            data: {
-                type: [Object, Array],
-                default() {
-                    return {}
-                }
+module.exports = {
+    props: {
+        value: {
+            type: [String, Number],
+            default: ''
+        },
+        color: {
+            type: String,
+            default: '#00a65a'
+        },
+        type: {
+            type: String,
+            default: 'radio',
+            validator: function (value) {
+                return ['radio', 'button'].indexOf(value) !== -1
             }
         },
-        data() {
-            return {
-                items: {},
-                a: 'f3a7',//选中
-                b: 'f3a6'//未选
-            }
+        field: {
+            type: String,
+            default: ''
         },
-        watch: {
-            data: function (a, b) {
-                // console.log(a, b);
-                this.reItems();
-            }
+        label: {
+            type: String,
+            default: ''
         },
-        created() {
-            this.reItems();
+        name: {
+            type: String,
+            default: ''
         },
-        methods: {
-            clickRadio(val) {
-                this.$emit('input', val);
-                this.$emit('click', val);
-            },
-            reItems() {
-                this.items = {};
-                if (this.data.constructor === Array) {
-                    this.data.forEach(ds => {
-                        this.items[ds] = ds;
-                        // this.items[ds[this.field]] = ds[this.label]
-                    })
-                } else {
-                    if (this.field) {
-                        for (let tm in this.data) {
-                            // console.log(this.data[tm], this.field, this.label);
-                            this.items[this.data[tm][this.field]] = this.data[tm][this.label]
-                        }
-                    } else {
-                        this.items = this.data;
-                    }
-
-                }
-
+        data: {
+            type: [Object, Array],
+            default() {
+                return {}
             }
         }
+    },
+    data() {
+        return {
+            items: {},
+            a: 'f3a7',//选中
+            b: 'f3a6'//未选
+        }
+    },
+    watch: {
+        data: function (a, b) {
+            // console.log(a, b);
+            this.reItems();
+        }
+    },
+    created() {
+        this.reItems();
+    },
+    methods: {
+        clickRadio(val) {
+            this.$emit('input', val);
+            this.$emit('click', val);
+        },
+        reItems() {
+            this.items = {};
+            if (this.data.constructor === Array) {
+                if (this.field) {
+                    this.data.forEach(ds => {
+                        this.items[ds[this.field]] = ds[this.label]
+                    })
+                } else {
+                    this.data.forEach(ds => {
+                        this.items[ds] = ds;
+                    })
+                }
+            } else {
+                if (this.field) {
+                    for (let tm in this.data) {
+                        // console.log(this.data[tm], this.field, this.label);
+                        this.items[this.data[tm][this.field]] = this.data[tm][this.label]
+                    }
+                } else {
+                    this.items = this.data;
+                }
+
+            }
+
+        }
     }
+}
 </script>
