09b69f6e7b81401a74cfc279258cd22370b79953
laocc <fazo@qq.com>
2021-09-10 00:32:26 +0800

    Signed-off-by: laocc <fazo@qq.com>

--- components/db-copy.vue
+++ components/db-copy.vue
@@ -1,107 +1,115 @@
 <template>
-    <span class="cpspan" @click="copyA" :class="{unselect:!select}">
-        <i v-if="before||label">{{ before || label }}</i><i>
+    <span class="cpspan" :style="{display:block}" @click="copyA" :class="{unselect:!select}">
+        <i :style="label_css" v-if="before||label">{{ before || label }}</i><i>
         <slot></slot>
         </i><em v-if="cpText && clk && icon" :class="ico" @click.stop="copy"></em>
     </span>
 </template>
 
 <script>
-module.exports = {
-    name: "db-copy",
-    props: {
-        label: {//前置label，不被复制
-            type: String,
-            default: ''
+    module.exports = {
+        name: "db-copy",
+        props: {
+            label: {//前置label，不被复制
+                type: String,
+                default: ''
+            },
+            before: {//前置label，不被复制
+                type: String,
+                default: ''
+            },
+            label_css: {//前置label式样
+                type: String,
+                default: ''
+            },
+            after: {
+                type: String,
+                default: ''
+            },
+            block: {
+                type: String,
+                default: 'inline-block'
+            },
+            text: {//实际要复制的内容
+                type: [String, Number],
+                default: ''
+            },
+            full: {//点击整个内容就复制
+                type: [Boolean, String, Number],
+                default: true
+            },
+            icon: {//是否显示复制icon
+                type: Boolean,
+                default: true
+            },
+            select: {//文本是否允许被选择
+                type: Boolean,
+                default: false
+            },
         },
-        before: {//前置label，不被复制
-            type: String,
-            default: ''
+        data() {
+            return {
+                ico: 'fc f3ef',
+                cpText: null,
+                clk: true,
+            }
         },
-        after: {
-            type: String,
-            default: ''
+        created() {
+            this.cpText = (this.text ? this.text : this.$slots.default[0].text.trim()) + '';
         },
-        text: {//实际要复制的内容
-            type: [String, Number],
-            default: ''
+        updated() {
+            this.cpText = (this.text ? this.text : this.$slots.default[0].text.trim()) + '';
         },
-        full: {//点击整个内容就复制
-            type: [Boolean, String, Number],
-            default: true
-        },
-        icon: {//是否显示复制icon
-            type: Boolean,
-            default: true
-        },
-        select: {//文本是否允许被选择
-            type: Boolean,
-            default: false
-        },
-    },
-    data() {
-        return {
-            ico: 'fc f3ef',
-            cpText: null,
-            clk: true,
-        }
-    },
-    created() {
-        this.cpText = (this.text ? this.text : this.$slots.default[0].text.trim()) + '';
-    },
-    updated() {
-        this.cpText = (this.text ? this.text : this.$slots.default[0].text.trim()) + '';
-    },
-    methods: {
-        copyA() {
-            if (this.full || !this.icon) this.copy();
-        },
-        copy() {
-            if (!this.cpText) return;
-            this.cpText.replace(/<br>/g, "\r\n").copy((obj) => {
-                this.$notify({
-                    title: '复制成功',
-                    message: obj,
-                    type: 'success',
-                    duration: 2000
+        methods: {
+            copyA() {
+                if (this.full || !this.icon) this.copy();
+            },
+            copy() {
+                if (!this.cpText) return;
+                this.cpText.replace(/<br>/g, "\r\n").copy((obj) => {
+                    this.$notify({
+                        title: '复制成功',
+                        message: obj,
+                        type: 'success',
+                        duration: 2000
+                    });
                 });
-            });
+            }
         }
     }
-}
 </script>
 
 <style scoped>
-.cpspan {
-    display: block;
-    clear: both;
-    cursor: default;
-    overflow: hidden;
-    padding-left: 0;
-    position: relative;
-}
+    .cpspan {
+        display: inline-block;
+        clear: both;
+        cursor: default;
+        overflow: hidden;
+        padding-left: 0;
+        position: relative;
+    }
 
-.cpspan:hover {
-    color: #2a57ff;
-}
+    .cpspan:hover {
+        color: #2a57ff;
+    }
 
-.cpspan:hover em {
-    display: inline-block;
-}
+    .cpspan:hover em {
+        display: inline-block;
+    }
 
-em {
-    display: none;
-    margin-left: 3px;
-    color: #2a57ff;
-}
+    em {
+        display: none;
+        margin-left: 3px;
+        color: #2a57ff;
+    }
 
-em:hover {
-    color: #ff2300;
-}
+    em:hover {
+        color: #ff2300;
+    }
 
-i {
-    display: inline;
-}
+    i {
+        display: inline;
+    }
 
 
 </style>
