02465419a59901afa694761701df8446ded1daf0
LaoCC <fazo@qq.com>
2021-11-02 01:24:53 +0800

    Signed-off-by: LaoCC <fazo@qq.com>

--- components/db-checkbox.vue
+++ components/db-checkbox.vue
@@ -1,231 +1,232 @@
 <template>
-    <ul>
-        <li class="prepend" v-if="slotPrepend">
-            <slot name="prepend"></slot>
-        </li>
-        <li v-for="(lab,key) in items" @click="clk(key)"
-            v-show="lab!==undefined"
-            :style="'color:'+(key.in_array(item)?color:'')">
-            <em class="fa" :class="(key.in_array(item)?b:a)"></em>
-            <span>{{ lab }}</span>
-        </li>
-        <input type="hidden" :name="name" :value="joinValue">
-        <li class="space"></li>
-        <li class="append" v-if="slotAppend">
-            <slot name="append"></slot>
-        </li>
-    </ul>
+  <ul>
+    <li class="prepend" v-if="slotPrepend">
+      <slot name="prepend"></slot>
+    </li>
+    <li v-for="(lab,key) in items" @click="clk(key)"
+        v-show="lab!==undefined"
+        :style="'color:'+(key.in_array(item)?color:'')">
+      <em class="fa" :class="(key.in_array(item)?b:a)"></em>
+      <span>{{ lab }}</span>
+    </li>
+    <input type="hidden" :name="name" :value="joinValue">
+    <li class="space"></li>
+    <li class="append" v-if="slotAppend">
+      <slot name="append"></slot>
+    </li>
+  </ul>
 </template>
 
 <style scoped>
-    ul {
-        display: flex;
-        flex: 1;
-        flex-wrap: wrap;
-    }
+ul {
+  display: flex;
+  flex: 1;
+  flex-wrap: wrap;
+  /*background-color: #fff;*/
+}
 
-    ul li {
-        padding: 5px 16px 5px 6px;
-        cursor: pointer;
-        vertical-align: middle;
-        text-align: center;
-        align-items: center;
-        display: flex;
-        justify-content: center;
-        border-radius: 1px;
-        background: #ffffff;
-        min-height: 32px;
-        font-size: 14px;
-    }
+ul li {
+  padding: 5px 16px 5px 6px;
+  cursor: pointer;
+  vertical-align: middle;
+  text-align: center;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  border-radius: 1px;
+  background: transparent;
+  min-height: 32px;
+  font-size: 14px;
+}
 
-    ul li:hover {
-        background: #f0f0f0;
-    }
+ul li:hover {
+  background: #f0f0f0;
+}
 
-    em {
-        font-size: 16px;
-        border: 0;
-        border-radius: 0;
-    }
+em {
+  font-size: 16px;
+  border: 0;
+  border-radius: 0;
+}
 
-    span {
-        margin-left: 5px;
+span {
+  margin-left: 5px;
 
-    }
+}
 
-    .space {
-        flex: 1;
-    }
+.space {
+  flex: 1;
+}
 
-    .prepend {
-        background: #F5F7FA;
-        width: 105px;
-    }
+.prepend {
+  background: #F5F7FA;
+  width: 105px;
+}
 
-    .append {
-        background: #F5F7FA;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        padding: 0 10px;
-        color: #888;
-        border-left:1px solid #DCDFE6;
-    }
+.append {
+  background: #F5F7FA;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 10px;
+  color: #888;
+  border-left: 1px solid #DCDFE6;
+}
 
 </style>
 
 <script>
-    module.exports = {
-        props: {
-            value: {
-                type: [Array, String, Number],
-                default() {
-                    return []
-                }
-            },
-            color: {
-                type: String,
-                default: '#00a65a'
-            },
-            type: {//值数据类型
-                type: String,
-                default: 'string'
-            },
-            field: {
-                type: String,
-                default: ''
-            },
-            label: {
-                type: String,
-                default: ''
-            },
-            min: {
-                type: Number,
-                default: 0
-            },
-            max: {
-                type: Number,
-                default: 0
-            },
-            name: {
-                type: String,
-                default: ''
-            },
-            data: {
-                type: [Object, Array],
-                default() {
-                    return {}
-                }
-            }
-        },
-        data() {
-            return {
-                items: {},
-                item: [],
-                a: 'e651',
-                b: 'e652',
-                once: (!!this.$slots.default),
-                slotPrepend: Boolean(this.$slots.prepend),
-                slotAppend: Boolean(this.$slots.append),
-            }
-        },
-        created() {
-            if (typeof this.value === 'object') {
-                this.item = this.value;
-            } else {
-                this.item = [this.value];
-            }
-            this.reItems();
-        },
-        computed: {
-            joinValue: function () {
-                if (this.type === 'int') {
-                    this.item.forEach((v, i) => {
-                        this.item[i] = parseInt(v);
-                    })
-                }
-                if (typeof this.value === 'object') {
-                    return JSON.stringify(this.item)
-                } else {
-                    return this.item.join(',');
-                }
-            }
-        },
-        watch: {
-            value: function (a, b) {
-                if (typeof a === 'object') {
-                    this.item = a;
-                } else {
-                    this.item = [a];
-                }
-            },
-            data: function (a, b) {
-                this.reItems();
-            }
-        },
-        methods: {
-            reItems() {
-                this.items = {};
-                if (this.data.constructor === Array) {
-                    this.data.forEach(ds => {
-                        if (this.field) {
-                            this.items[ds[this.field]] = ds[this.label]
-                        } else {
-                            this.items[ds] = ds;
-                        }
-                    })
-                } else {
-                    if (this.field) {
-                        for (let tm in this.data) {
-                            // console.log(this.data[tm], this.field, this.label);
-                            this.items[this.data[tm][this.field]] = this.data[tm][this.label]
-                        }
-                    } else {
-                        this.items = this.data;
-                    }
-                }
-                //过滤掉不在可选项中的值
-                this.item = this.item.filter(k => this.items[k] !== undefined);
-            },
-            updateData(value) {
-                //受理外部控制
-                this.items = value;
-            },
-            updateValue(value) {
-                //受理外部控制
-                if (typeof value === 'object') {
-                    this.item = value;
-                } else {
-                    this.item = [value];
-                }
-            },
-            clk(v) {
-                if (this.type === 'int') v = parseInt(v);
-                let add = true;
-                if (v.in_array(this.item)) {
-                    // this.item.splice(this.item.indexOf(v), 1);
-                    this.item = this.item.filter(k => k !== v);
-                    add = false;
-                } else {
-                    if (this.max > 0 && this.item.length >= this.max) {
-                        this.$message.error(`最多可选择${this.max}个选项`);
-                        return;
-                    }
-                    add = true;
-                    //只有一个目标元素时，若是选中，则清空原来的数据，这只是临时变通办法，有待优化
-                    if (Object.keys(this.items).length === 1) this.item = [];
-                    this.item.push(v);
-                }
-
-                if (typeof this.value === 'object') {
-                    this.$emit('click', this.item, v, add);
-                    this.$emit('input', this.item)
-                } else {
-                    this.$emit('click', this.item.join(','), v, add);
-                    this.$emit('input', this.item.join(','))
-                }
-
-            }
-        }
+module.exports = {
+  props: {
+    value: {
+      type: [Array, String, Number],
+      default() {
+        return []
+      }
+    },
+    color: {
+      type: String,
+      default: '#00a65a'
+    },
+    type: {//值数据类型
+      type: String,
+      default: 'string'
+    },
+    field: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    min: {
+      type: Number,
+      default: 0
+    },
+    max: {
+      type: Number,
+      default: 0
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    data: {
+      type: [Object, Array],
+      default() {
+        return {}
+      }
     }
+  },
+  data() {
+    return {
+      items: {},
+      item: [],
+      a: 'e651',
+      b: 'e652',
+      once: (!!this.$slots.default),
+      slotPrepend: Boolean(this.$slots.prepend),
+      slotAppend: Boolean(this.$slots.append),
+    }
+  },
+  created() {
+    if (typeof this.value === 'object') {
+      this.item = this.value;
+    } else {
+      this.item = [this.value];
+    }
+    this.reItems();
+  },
+  computed: {
+    joinValue: function () {
+      if (this.type === 'int') {
+        this.item.forEach((v, i) => {
+          this.item[i] = parseInt(v);
+        })
+      }
+      if (typeof this.value === 'object') {
+        return JSON.stringify(this.item)
+      } else {
+        return this.item.join(',');
+      }
+    }
+  },
+  watch: {
+    value: function (a, b) {
+      if (typeof a === 'object') {
+        this.item = a;
+      } else {
+        this.item = [a];
+      }
+    },
+    data: function (a, b) {
+      this.reItems();
+    }
+  },
+  methods: {
+    reItems() {
+      this.items = {};
+      if (this.data.constructor === Array) {
+        this.data.forEach(ds => {
+          if (this.field) {
+            this.items[ds[this.field]] = ds[this.label]
+          } else {
+            this.items[ds] = ds;
+          }
+        })
+      } else {
+        if (this.field) {
+          for (let tm in this.data) {
+            // console.log(this.data[tm], this.field, this.label);
+            this.items[this.data[tm][this.field]] = this.data[tm][this.label]
+          }
+        } else {
+          this.items = this.data;
+        }
+      }
+      //过滤掉不在可选项中的值
+      this.item = this.item.filter(k => this.items[k] !== undefined);
+    },
+    updateData(value) {
+      //受理外部控制
+      this.items = value;
+    },
+    updateValue(value) {
+      //受理外部控制
+      if (typeof value === 'object') {
+        this.item = value;
+      } else {
+        this.item = [value];
+      }
+    },
+    clk(v) {
+      if (this.type === 'int') v = parseInt(v);
+      let add = true;
+      if (v.in_array(this.item)) {
+        // this.item.splice(this.item.indexOf(v), 1);
+        this.item = this.item.filter(k => k !== v);
+        add = false;
+      } else {
+        if (this.max > 0 && this.item.length >= this.max) {
+          this.$message.error(`最多可选择${this.max}个选项`);
+          return;
+        }
+        add = true;
+        //只有一个目标元素时，若是选中，则清空原来的数据，这只是临时变通办法，有待优化
+        if (Object.keys(this.items).length === 1) this.item = [];
+        this.item.push(v);
+      }
+
+      if (typeof this.value === 'object') {
+        this.$emit('click', this.item, v, add);
+        this.$emit('input', this.item)
+      } else {
+        this.$emit('click', this.item.join(','), v, add);
+        this.$emit('input', this.item.join(','))
+      }
+
+    }
+  }
+}
 </script>
--- components/db-paging.vue
+++ components/db-paging.vue
@@ -19,188 +19,204 @@
             </el-dropdown-menu>
         </el-dropdown>
 
+        <li class="lab" v-if="sumCount">
+            <span class="sum" v-for="(sm,si) in data.sum">{{ si }}:{{ sm }}</span>
+        </li>
+
         <li style="flex: 1;"></li>
     </ul>
 </template>
 
 <script>
-    module.exports = {
-        props: {
-            sizeOption: {
-                type: Array,
-                default() {
-                    return [];
-                }
-            },
-            data: {
-                type: Object,
-                default() {
-                    return {
-                        index: 1,
-                        recode: 0,
-                        size: 10,
-                        key: 'page',
-                        total: 1,
-                    };
-                }
-            },
-        },
-        data() {
-            return {
-                link: true,
-                param: '_=1',
-                hasSlot: Boolean(this.$slots.default),
-            };
-        },
-        created() {
-            this.link = !this._events.click;
-            let mch = window.location.search.substr(1).match(/([\w\-\.]+=[^&]+)+/ig);
-            if (mch !== null) {
-                let params = [];
-                mch.forEach(pm => {
-                    let p = pm.split(/([\w\-\.]+)=(.+)/i);
-                    if (p[1] !== this.data.key) params[p[1]] = decodeURI(p[2]);
-                });
-                this.param = params.join('&');
+module.exports = {
+    props: {
+        sizeOption: {
+            type: Array,
+            default() {
+                return [];
             }
-            console.log('this.option', this.txt, this.opt, this.data);
-
         },
-        computed: {
-            sizeOptions() {
-                if (this.sizeOption.length > 0) return this.sizeOption;
-                let opt = [];
-                for (let i = 1; i <= 4; i++) opt.push(i * 10);
-                for (let j = 1; j <= 6; j++) opt.push(j * 50);
-                return opt;
-            },
-            sizeLabel: function () {
-                let p = [], val = this.data;
+        data: {
+            type: Object,
+            default() {
+                return {
+                    index: 1,
+                    recode: 0,
+                    size: 10,
+                    key: 'page',
+                    sum: {},
+                    total: 1,
+                };
+            }
+        },
+    },
+    data() {
+        return {
+            link: true,
+            param: '_=1',
+            hasSlot: Boolean(this.$slots.default),
+        };
+    },
+    created() {
+        this.link = !this._events.click;
+        let mch = window.location.search.substr(1).match(/([\w\-\.]+=[^&]+)+/ig);
+        if (mch !== null) {
+            let params = [];
+            mch.forEach(pm => {
+                let p = pm.split(/([\w\-\.]+)=(.+)/i);
+                if (p[1] !== this.data.key) params[p[1]] = decodeURI(p[2]);
+            });
+            this.param = params.join('&');
+        }
+        console.log('this.option', this.txt, this.opt, this.data);
 
-                if (val.total < 2) {
-                    p.push(1);//只有一页
-                } else if (val.total < 10) {
-                    //少于10页全显示
-                    for (let i = 1; i <= val.total; i++) p.push(i);
+    },
+    computed: {
+        sizeOptions() {
+            if (this.sizeOption.length > 0) return this.sizeOption;
+            let opt = [];
+            for (let i = 1; i <= 4; i++) opt.push(i * 10);
+            for (let j = 1; j <= 6; j++) opt.push(j * 50);
+            return opt;
+        },
+        sumCount: function () {
+            for (let i in this.data.sum) { // 如果不为空，则会执行到这一步，返回true
+                return true
+            }
+            return false // 如果为空,返回false
+        },
+        sizeLabel: function () {
+            let p = [], val = this.data;
+
+            if (val.total < 2) {
+                p.push(1);//只有一页
+            } else if (val.total < 10) {
+                //少于10页全显示
+                for (let i = 1; i <= val.total; i++) p.push(i);
+            } else {
+                if (val.index < 5) {
+                    //前5页：前1234567.L后 =9
+                    for (let i = 1; i < val.index; i++) p.push(i);
+                    p.push(val.index);
+                    for (let i = val.index + 1; i <= 7; i++) p.push(i);
+                    p.push('…');
+                    p.push(val.total);
+
+                } else if (val.index > (val.total - 5)) {
+                    //第5页开始：前1.12021.L后 = 11
+                    p.push(1);
+                    p.push('…');
+                    for (let i = val.total - 6; i <= val.total; i++) p.push(i);
+
                 } else {
-                    if (val.index < 5) {
-                        //前5页：前1234567.L后 =9
-                        for (let i = 1; i < val.index; i++) p.push(i);
-                        p.push(val.index);
-                        for (let i = val.index + 1; i <= 7; i++) p.push(i);
-                        p.push('…');
-                        p.push(val.total);
-
-                    } else if (val.index > (val.total - 5)) {
-                        //第5页开始：前1.12021.L后 = 11
-                        p.push(1);
-                        p.push('…');
-                        for (let i = val.total - 6; i <= val.total; i++) p.push(i);
-
-                    } else {
-                        //最后5页：前1.2112345后 = 11
-                        p.push(1);
-                        p.push('…');
-                        for (let i = val.index - 2; i <= val.index + 2; i++) p.push(i);
-                        p.push('…');
-                        p.push(val.total);
-                    }
+                    //最后5页：前1.2112345后 = 11
+                    p.push(1);
+                    p.push('…');
+                    for (let i = val.index - 2; i <= val.index + 2; i++) p.push(i);
+                    p.push('…');
+                    p.push(val.total);
                 }
-                return p;
             }
+            return p;
+        }
+    },
+    methods: {
+        rePageSize(v) {
+            console.log(v);
+            this.data.size = parseInt(v);
+            this.click(0);
         },
-        methods: {
-            rePageSize(v) {
-                console.log(v);
-                this.data.size = parseInt(v);
-                this.click(0);
-            },
-            url(i) {
-                if (i === 'p') {
-                    i = this.data.index - 1;
-                    if (i < 1) i = 1;
-                } else if (i === 'n') {
-                    i = this.data.index + 1;
-                    if (i > this.data.total) i = this.data.total;
-                }
-                return `?` + this.param + `\&${this.data.key}=${i}\&total=${this.data.total}`;
-            },
-            click(i) {
-                console.log(i);
-                if (i === 'p') {
-                    i = this.data.index - 1;
-                    if (i < 1) i = 1;
-                } else if (i === 'n') {
-                    i = this.data.index + 1;
-                    if (i > this.data.total) i = this.data.total;
-                }
-                if (i === this.data.index) return false;
-
-                if (!this._events.click) {
-                    console.log('未定义 @click');
-                    return false;
-                }
-                let param = this.param;
-                param[this.data.key] = i;
-                let value = {index: i, size: this.data.size, url: this.url(i), param: param, total: this.data.recode};
-                console.log('pageClick:', value);
-                this.$emit('click', value);
+        url(i) {
+            if (i === 'p') {
+                i = this.data.index - 1;
+                if (i < 1) i = 1;
+            } else if (i === 'n') {
+                i = this.data.index + 1;
+                if (i > this.data.total) i = this.data.total;
             }
+            return `?` + this.param + `\&${this.data.key}=${i}\&total=${this.data.total}`;
+        },
+        click(i) {
+            console.log(i);
+            if (i === 'p') {
+                i = this.data.index - 1;
+                if (i < 1) i = 1;
+            } else if (i === 'n') {
+                i = this.data.index + 1;
+                if (i > this.data.total) i = this.data.total;
+            }
+            if (i === this.data.index) return false;
+
+            if (!this._events.click) {
+                console.log('未定义 @click');
+                return false;
+            }
+            let param = this.param;
+            param[this.data.key] = i;
+            let value = {index: i, size: this.data.size, url: this.url(i), param: param, total: this.data.recode};
+            console.log('pageClick:', value);
+            this.$emit('click', value);
         }
     }
+}
 </script>
 
 <style scoped>
-    .paging {
-        width: 100%;
-        display: flex;
-        border-radius: 3px;
-        border: 1px solid #ddd;
-    }
+.paging {
+    width: 100%;
+    display: flex;
+    border-radius: 3px;
+    border: 1px solid #ddd;
+}
 
-    .btn {
-        padding: 5px 10px;
-        display: block;
-    }
+.btn {
+    padding: 5px 10px;
+    display: block;
+}
 
-    .lab {
-        padding-left: 10px;
-        padding-right: 10px;
-        display: flex;
-        align-items: center;
-        min-width: 38px;
-        text-align: center;
-        cursor: pointer;
-    }
+.lab {
+    padding-left: 10px;
+    padding-right: 10px;
+    display: flex;
+    align-items: center;
+    min-width: 38px;
+    text-align: center;
+    cursor: pointer;
+}
 
-    .num {
-        background: #fafafa;
-        border-right: 1px solid #ddd;
-        display: flex;
-        align-items: center;
-        height: 34px;
-        line-height: 34px;
-        min-width: 38px;
-        text-align: center;
-    }
+.num {
+    background: #fafafa;
+    border-right: 1px solid #ddd;
+    display: flex;
+    align-items: center;
+    height: 34px;
+    line-height: 34px;
+    min-width: 38px;
+    text-align: center;
+}
 
-    a {
-        padding-left: 10px;
-        padding-right: 10px;
-        display: inline-block;
-        flex: 1;
-        width: 100%;
-        height: 100%;
-        /*color: #fff !important;*/
-    }
+.sum {
+    margin: 0 3px;
+    color: #3660a6;
+}
 
-    a:hover {
-        background: #dfdfdf;
-    }
+a {
+    padding-left: 10px;
+    padding-right: 10px;
+    display: inline-block;
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    /*color: #fff !important;*/
+}
 
-    .active, .active a {
-        background: #1471f1;
-        color: #fff;
-    }
+a:hover {
+    background: #dfdfdf;
+}
+
+.active, .active a {
+    background: #1471f1;
+    color: #fff;
+}
 
 </style>
